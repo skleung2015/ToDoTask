d95f2a146c19b2de66276122b7a41fdc0abd2637
Terence <terencsk@sg.ibm.com>
2018-06-01 11:40:57 +0800

    webpack

--- app/src/index.js
+++ app/src/index.js
@@ -1,74 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ToDoItems from "./ToDoItems";
-import './index.css';
+import React from "react"
+import ToDoItems from "./ToDoItems"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-<h1 style="color:red;text-align:center;">To Do List></h1>
+class TaskForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
 
-
-class TaskForm extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      tasks: []
-    };
-
-    this.addTask = this.addTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
-  }
-
-  addTask(event){
-    if(this.inputElement.value !== ""){
-      var newTask = {
-        text: this.inputElement.value,
-        key: Date.now()
-      };
-      this.setState((prevState) => {
-        return{
-          tasks: prevState.tasks.concat(newTask)
-        };
-      });
+        this.addTask = this.addTask.bind(this)
+        this.deleteTask = this.deleteTask.bind(this)
     }
 
-    this.inputElement.value = "";
-    console.log(this.state.tasks);
-    event.preventDefault();
-  }
+    addTask(event) {
+        if (this.inputElement.value !== "") {
+            const newTask = {
+                text: this.inputElement.value,
+                key: Date.now()
+            }
+            this.setState(prevState => ({
+                tasks: prevState.tasks.concat(newTask)
+            }))
+        }
 
-  deleteTask(key){
-    var filteredItems = this.state.tasks.filter(function(item){
-      return (item.key !== key)
-    });
+        this.inputElement.value = ""
+        console.log(this.state.tasks)
+        event.preventDefault()
+    }
 
-    this.setState({
-      tasks: filteredItems
-    });
-  }
+    deleteTask(key) {
+        const filteredItems = this.state.tasks.filter(item => item.key !== key)
 
+        this.setState({
+            tasks: filteredItems
+        })
+    }
 
-  render(){
-    return(
-      <div className="toDoInterface">
-        <div className="header">
-          <form onSubmit = {this.addTask}>
-          Task:
-          <input type="text" ref={(a) => this.inputElement = a}/>
-          <button type = "submit">Add</button>
-          </form>
-        </div>
+    render() {
+        return (
+            <div className="toDoInterface">
+                <div className="header">
+                    <form onSubmit={this.addTask}>
+                        Task:
+                        <input type="text" ref={a => (this.inputElement = a)} />
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
 
-        <div>
-          <ToDoItems entries = {this.state.tasks}
-                      delete = {this.deleteTask}/>
-        </div>
-    </div>
-
-    );
-  }
+                <div>
+                    <ToDoItems
+                        entries={this.state.tasks}
+                        delete={this.deleteTask}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
-
-ReactDOM.render(
-  <TaskForm />,
-  document.getElementById('root')
-);
+ReactDOM.render(<TaskForm />, document.getElementById("root"))
---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const app = express()
+
+// Loading the environment port with default fallbacks
+const HTTP_PORT = process.env.PORT || 3000
+
+// Serve static assets
+app.use(express.static(path.join(__dirname, "..", "dist")))
+
+// mount parser for applicaton/json content
+app.use(bodyParser.json({ limit: "100mb" }))
+
+// reroute all frontend routes to be handled by react-router
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
+})
+
+// Start the app
+app.listen(HTTP_PORT, () => {
+    console.log(`Listening on port ${HTTP_PORT}`)
+})
